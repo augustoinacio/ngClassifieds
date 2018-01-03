b74000a738d73a0443d3335a7c970460d70ee7a8
Augusto Inacio <augustoinacio@gmail.com>
2018-01-03 00:42:51 -0200

    escopo inicial do projeto

---
+++ components/classifieds.ctr.js
@@ -0,0 +1,10 @@
+(function(){
+    "use strict";
+    angular
+    .module('ngClassifieds')
+    .controller('classifiedCtrl', function($scope){
+        $scope.name = "augusto";
+        $scope.message = "Hello World!!!";
+        console.log('teste');
+    })    
+})();
